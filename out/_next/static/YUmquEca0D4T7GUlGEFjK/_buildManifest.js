359feaea09ce88fdf0e0846ec168e94cc34260a8
fermx3 <99512170+fermx3@users.noreply.github.com>
2023-08-31 14:50:59 -0600

    Delete out directory

--- out/_next/static/YUmquEca0D4T7GUlGEFjK/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/262-b482c64be994f328.js","static/css/f78e704cda61296d.css","static/chunks/pages/index-b2185b5613637bc7.js"],"/_error":["static/chunks/pages/_error-e4f561a102d9bb14.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
